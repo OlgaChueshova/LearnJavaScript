a1df818b34826d21568d1330981461e18fc17c80
Chueshova Olga <chueshova_olga@mail.ru>
2022-10-11 08:00:01 +0300

    hw-7

---
+++ lesson-7/homework-7/index.js
@@ -0,0 +1,45 @@
+function Worker(name, surname, rate, days) {
+    this.name = name;
+    this.surname = surname;
+    this.rate = rate;
+    this.days = days;
+    this.getSalary = function () {
+        return `salary: ${rate * days}`;
+    }
+}
+
+const worker = new Worker('Ольга', 'Чуешова', 30, 20);
+
+console.log(worker.name);
+console.log(worker.surname);
+console.log(worker.rate);
+console.log(worker.days);
+console.log(worker.getSalary());
+
+
+class MyString {
+    constructor(word) {
+        this.word = word;
+    }
+    reverse() {
+        return this.word
+            .split('')
+            .reverse()
+            .join('')
+    }
+    ucFirst() {
+        return this.word[0].toUpperCase() + this.word.slice(1)
+    }
+    ucWords() {
+        return this.word
+            .split(' ')
+            .map(letter => letter[0].toUpperCase() + letter.slice(1, letter.length))
+            .join(' ')
+    }
+}
+
+const str = new MyString('abcde');
+
+console.log(str.reverse('abcde'));
+console.log(str.ucFirst('abcde'));
+console.log(str.ucWords('abcde abcde abcde'));
