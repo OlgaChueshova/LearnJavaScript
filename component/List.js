4d164d9d3b77ed3b7b232dbdd7c3aba058fb8bf2
Chueshova Olga <chueshova_olga@mail.ru>
2022-11-15 07:56:34 +0300

    add list

--- component/List.js
+++ component/List.js
@@ -31,7 +31,7 @@ export class List extends HTMLElement {
             {
                 label: 'Все контакты',
                 href: '//'
-            },   
+            },
             {
                 label: 'Нижний Новгород',
                 href: '//',
@@ -53,24 +53,30 @@ export class List extends HTMLElement {
                 href: 'Mailto:info@futureacademy.ru'
             },
         ];
+        this.state = {
+            display: 'none'
+        }
     }
 
     removeSelf() {
-        window.addEventListener('click', () => {
+        const header = document.querySelector('.header-mobile');
+        // const list = evt.target.closest('.mobile-navigation');
+        header.addEventListener('click', () => {
             if (!this) {
-                this.remove();
+                return this.remove();
             }
         })
     }
 
     connectedCallback() {
-        const target = document.querySelector('.header__navigation--dropdown');
-        window.addEventListener('click', (evt) => {
-            if(target) {
+        const header = document.querySelector('.header-mobile');
+        const dropdown = document.querySelector('.header__navigation--dropdown');
+        header.addEventListener('click', () => {
+            if (dropdown) {
                 return this.render();
             }
+            this.removeSelf();
         })
-        this.removeSelf();
     }
 
     disconnectedCallback() {
@@ -91,115 +97,3 @@ customElements.define('it-list', List)
 
 
 
-// this.links = {
-//     allCourses: {
-//         label: 'Все курсы',
-//         subLinks: [
-//             {
-//                 label: 'Детям',
-//                 subSubLinks: ['Робототехника', 'Создание игр', 'Программирование', 'Мультимедиа', 'Шахматы', 'Блогинг']
-//             },
-//             {
-//                 label: 'Подросткам',
-//                 subSubLinks: ['WEB-разработка', 'Графическое моделирование', 'Game Development', 'Soft skills', 'Шахматы', 'Блогинг']
-//             },
-//             {
-//                 label: 'Взрослым',
-//                 subSubLinks: ['Разработчик игр на Unity', 'Графическй дизайн', 'Тестировщик ПО', 'JAVA-разработчик', 'IOS-разработчик', 'Phyton-разработчик']
-//             },
-//         ]
-//     },
-//     otherLinks: [
-//         {
-//         label: 'Об академии',
-//         href: '//'
-//         },
-//         {
-//         label: 'Мероприятия',
-//         href: '//'
-//         },
-//         {
-//         label: 'Новости',
-//         href: '//'
-//         },
-//         {
-//         label: 'База знаний',
-//         href: '//'
-//         },
-//         {
-//         label: 'Карьера',
-//         href: '//'
-//         },
-//         {
-//         label: 'Все контакты',
-//         href: '//'
-//         },
-//     ],
-//     location:  {
-//         label: 'Нижний Новгород',
-//         subLinks: [
-//             {
-//                 label: 'Москва',
-//                 href: '//'
-//             },
-//             {
-//                 label: 'Ростов-на-Дону',
-//                 href: '//'
-//             },
-//             {
-//                 label: 'Санкт-Петербург',
-//                 href: '//'
-//             },
-//             {
-//                 label: 'Рязань',
-//                 href: '//'
-//             },
-//             {
-//                 label: 'Тюмень',
-//                 href: '//'
-//             },
-//         ]
-//     },
-//     signIn: {
-//         label: 'Войти',
-//         href: '//'
-//         },
-//     tel: {
-//         label: '8 800 950-33-98',
-//         href: 'tel:88009503398'
-//         },
-//     adress: {
-//         label: 'г.Москва, ул.Ленина, 50',
-//         href: ''
-//         },
-//     email: {
-//         label: 'info@futureacademy.ru',
-//         href: 'Mailto:info@futureacademy.ru'
-//         },
-//     networks: [
-//             {
-//                 label: './images/networks/vk.svg',
-//                 href: '//',
-//             },
-//             {
-//                 image: './images/networks/instagram.svg',
-//                 href: '//',
-//             },
-//             {
-//                 image: './images/networks/facebook.svg',
-//                 href: '//',
-//             },
-//             {
-//                 image: './images/networks/youtube.svg',
-//                 href: '//',
-//             },
-//             {
-//                 image: './images/networks/telegram.svg',
-//                 href: '//',
-//             },
-//         ],
-//     yandex: {
-//         label: './images/yandex.svg',
-//         href: '//',
-//         },
-// };
